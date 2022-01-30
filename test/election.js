f0e2df331aaee408b9ace24ca5a04ae153a34027
ANAND S <anands8196@gmail.com>
2022-01-30 15:55:11 +0530

    create Candidate unit test

--- test/election.js
+++ test/election.js
@@ -4,4 +4,21 @@ contract("Election", (accounts) => {
     it("Initializes two candidates", () => {
         return Election.deployed().then((i) => { return i.candidateCount() }).then((count) => { assert.equal(count, 2) })
     })
+    it("Initializes candidates with correct values", () => {
+        return Election.deployed().then((i) => {
+            electionInstance = i;
+            return electionInstance.candidates(0)
+        })
+            .then((candidate) => {
+                assert.equal(candidate[0], 0, "Contains InCorrect Id");
+                assert.equal(candidate[1], "C1", "Contains InCorrect Name");
+                assert.equal(candidate[2], 0, "Contains InCorrect Vote Count")
+                return electionInstance.candidates(1)
+            }).then((candidate) => {
+                assert.equal(candidate[0], 1, "Contains InCorrect Id");
+                assert.equal(candidate[1], "C2", "Contains InCorrect Name");
+                assert.equal(candidate[2], 0, "Contains InCorrect Vote Count")
+            })
+    })
+
 })
