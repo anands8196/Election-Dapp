2683b0eda21091666baa6a5e308752d0ea918425
ANAND S <anands8196@gmail.com>
2022-01-30 15:43:27 +0530

    create Candidate unit test

--- test/election.js
+++ test/election.js
@@ -1,7 +1,7 @@
 const Election = artifacts.require("./Election.sol");
 
-contract("Election", function (accounts) {
-    it("Initializes two candidates", function () {
+contract("Election", (accounts) => {
+    it("Initializes two candidates", () => {
         return Election.deployed().then((i) => { return i.candidateCount() }).then((count) => { assert.equal(count, 2) })
     })
 })
